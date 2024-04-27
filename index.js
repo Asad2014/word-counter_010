837f08c61e65448cd4bd55b9d6f506bc8b9cf0f0
asad2014 <asadkk2014@gmail.com>
2024-04-28 01:10:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.cyanBright("\n \t\t Welcome To Word-Counter"));
+console.log("=".repeat(50));
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+let words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence word counter is ${words.length}`);
